d83ada5926f6f8d027adf864cfd09d8a7b5048ec
enixiyo <quantumforwork@gmail.com>
2022-04-17 02:25:35 +0300

    add auth screen design

--- app/src/main/java/com/example/showcaseapp/MainActivity.kt
+++ app/src/main/java/com/example/showcaseapp/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.example.showcaseapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.showcaseapp.auth.AuthScreen
 import com.example.showcaseapp.ui.theme.ShowcaseAppTheme
-import com.github.zsoltk.compose.backpress.BackPressHandler
-import com.github.zsoltk.compose.backpress.LocalBackPressHandler
-import com.github.zsoltk.compose.router.Router
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,22 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AuthScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreviewX() {
-    ShowcaseAppTheme {
-        Greeting("Android")
     }
 }
